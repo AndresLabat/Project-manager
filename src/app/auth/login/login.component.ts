a16e60ab5908d90f885349b85fefc8f1dc8cda2b
Andrés Labat <135720098+AndresLabat@users.noreply.github.com>
2025-09-29 18:36:41 +0200

    feat: implement login functionality in LoginComponent with error handling

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrl: './login.scss'
 })
 export class LoginComponent {
+  username = '';
+  password = '';
+  error = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onLogin() {
+    if (this.authService.login(this.username, this.password)) {
+      this.router.navigate(['/projects']);
+    } else {
+      this.error = 'Incorrect username or password';
+    }
+  }
 
 }
