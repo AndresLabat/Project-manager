e94a91ac87cbce8d322e11b7468498990bdfb4f3
Andrés Labat <135720098+AndresLabat@users.noreply.github.com>
2025-09-30 15:09:03 +0200

    feat: enhance project form with success message display upon successful submission

--- src/app/projects/project-form/project-form.component.ts
+++ src/app/projects/project-form/project-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { ProjectsService } from '../projects.service';
 import { CommonModule } from '@angular/common';
@@ -9,9 +9,9 @@ import { CommonModule } from '@angular/common';
   templateUrl: './project-form.component.html',
   styleUrls: ['./project-form.component.scss']
 })
-
 export class ProjectFormComponent {
   form;
+  successMessage = signal('');
 
   constructor(private fb: FormBuilder, private projectsService: ProjectsService) {
     this.form = this.fb.group({
@@ -31,6 +31,7 @@ export class ProjectFormComponent {
     });
 
     this.form.reset();
+    this.successMessage.set('Project added successfully!');
+    setTimeout(() => this.successMessage.set(''), 3000);
   }
 }
-
