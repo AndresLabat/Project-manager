dcb4f23ccf74ee99835e2466739eed850c225e8c
Andrés Labat <135720098+AndresLabat@users.noreply.github.com>
2025-09-30 13:20:44 +0200

    feat: add header with title and logout button, and restructure app layout for improved user experience

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './auth/auth.service';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('project-manager');
+
+  title = signal('Project Manager');
+
+  constructor(private authService: AuthService, private router: Router, private titleService: Title) {
+    this.titleService.setTitle(this.title());
+  }
+
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
